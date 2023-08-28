dd5661660fb71a3d0747bed2c67b513c171999a3
pepega <aristockij@gmail.com>
2023-08-28 17:46:56 +0300

    fix lint

--- src/utils/resgiterComponent.ts
+++ src/utils/resgiterComponent.ts
@@ -4,7 +4,7 @@ import Block from "./Block";
 
 export function registerComponent(name: string, Component: typeof Block) {
   if (name in Handlebars.helpers) {
-    throw `The ${name} component is already registered!`;
+    throw new Error(`The ${name} component is already registered!`);
   }
 
   Handlebars.registerHelper(name,  (this: unknown, {hash, data, fn}: HelperOptions) => {
