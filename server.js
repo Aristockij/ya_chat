6c1ae55e999a803885f545914a8f2b94a11450f0
pepega <aristockij@gmail.com>
2023-09-18 17:59:49 +0300

    fix

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+import fallback from "express-history-api-fallback";
 import express from 'express';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const root = ${__dirname}/dist
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(`${__dirname}/dist/`));
+app.use(express.static(root));
+app.use(fallback("index.html", { root }));
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
